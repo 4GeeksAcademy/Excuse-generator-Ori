c5e0ecfa85a848d982596263753e7007dcc67160
Orianac0698 <150297944+Orianac0698@users.noreply.github.com>
2024-03-16 00:41:58 +0000

    Al haber realizado este commit , se extrajo lineas que estaban demas y sin funcionabilidad dentro del codigo. En html y js.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   let excuseGenerator = () => {
@@ -18,20 +14,19 @@ window.onload = () => {
       "while I was praying"
     ];
 
-    let whoInx = Math.floor(Math.random() * who.length);
-    let actionInx = Math.floor(Math.random() * action.length);
-    let whatInx = Math.floor(Math.random() * what.length);
-    let whenInx = Math.floor(Math.random() * when.length);
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
     return (
-      who[whoInx] +
+      who[whoIndex] +
       "" +
-      action[actionInx] +
+      action[actionIndex] +
       "" +
-      what[whatInx] +
+      what[whatIndex] +
       "" +
-      when[whenInx]
+      when[whenIndex]
     );
   };
   document.querySelector("#excuse").innerHTML = excuseGenerator();
-  console.log(excuseGenerator);
 };
